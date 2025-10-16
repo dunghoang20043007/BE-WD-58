ef14840571a7199b572e7147bb8bf82bfe45facc
phong <Phonglvph35411@fpt.edu.vn>
2025-10-16 21:00:32 +0700

    fix lỗi lấy sản phẩm không được

--- src/modules/product/product.service.js
+++ src/modules/product/product.service.js
@@ -1,8 +1,121 @@
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
-import { APIQuery } from "../../common/utils/APIQuery.js";
 import Product from "./models/product.model.js";
 import customResponse from "../../common/utils/customResponse.js";
 import { NotFoundError } from "../../errors/customerErr.js";
+import _ from "lodash";
+import mongoose, { Query, Document } from "mongoose";
+
+class APIQuery {
+    constructor(query, queryString) {
+        this.query = query;
+        this.queryString = queryString;
+    }
+
+    /**
+     * Filters the query based on the provided query parameters.
+     * It removes parameters that are not related to filtering (page, sort, limit, fields, search)
+     * and applies MongoDB operators (gte, gt, lte, lt) for filtering.
+     * @example /api/v1/products?price[gte]=100&price[lte]=500&name=iphone
+     */
+    filter() {
+        const queryObj = { ...this.queryString };
+        const excludedFields = ["page", "sort", "limit", "fields", "search"];
+        excludedFields.forEach((el) => delete queryObj[el]);
+        // Remove 'raw' fields from queryObj (remove manual fields)
+        Object.keys(queryObj).forEach((el) => {
+            if (el.includes("raw")) {
+                delete queryObj[el];
+            }
+        });
+        Object.keys(queryObj).forEach((el) => {
+            if (String(queryObj[el]).includes(",")) {
+                queryObj[el] = { $in: queryObj[el].split(",") };
+            }
+        });
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+        this.query = this.query.find(JSON.parse(queryStr));
+
+        return this;
+    }
+
+    /**
+     * Sorts the query results based on the provided query parameters.
+     * If no sort parameter is provided, it defaults to sorting by the creation date in descending order.
+     * @example /api/v1/products?sort=-price,rating,createdAt
+     */
+    sort() {
+        if (this.queryString.sort) {
+            const sortBy = this.queryString.sort.split(",").join(" ");
+            this.query = this.query.sort(sortBy);
+        } else {
+            this.query = this.query.sort("-createdAt");
+        }
+
+        return this;
+    }
+
+    /**
+     * Limits the fields returned in the query results based on the provided query parameters.
+     * If no fields parameter is provided, it excludes the '__v' field.
+     * @example /api/v1/products?fields=name,price,rating
+     */
+    limitFields() {
+        if (this.queryString.fields) {
+            const fields = this.queryString.fields.split(",").join(" ");
+            this.query = this.query.select(fields);
+        } else {
+            this.query = this.query.select("-__v");
+        }
+
+        return this;
+    }
+
+    /**
+     * Searches the query results based on the provided search parameter.
+     * It checks if the search parameter is a valid ObjectId. If so, it searches by _id,
+     * otherwise, it searches by name using a regular expression.
+     * @example /api/v1/products?search=iphone
+     */
+    search() {
+        if (this.queryString.search) {
+            const isId = mongoose.Types.ObjectId.isValid(this.queryString.search);
+            if (isId) {
+                const search = this.queryString.search;
+                this.query = this.query.find({ _id: search });
+            } else {
+                const search = _.toLower(this.queryString.search);
+                this.query = this.query.find({ name: { $regex: search, $options: "i" } });
+            }
+        }
+        return this;
+    }
+
+    /**
+     * Paginates the query results based on the provided page and limit parameters.
+     * Defaults to page 1 and limit 10 if not provided.
+     * @example /api/v1/products?page=2&limit=20
+     */
+    paginate() {
+        const page = Number(this.queryString.page) || 1;
+        const limit = Number(this.queryString.limit) || 10;
+        const skip = (page - 1) * limit;
+
+        this.query = this.query.skip(skip).limit(limit);
+
+        return this;
+    }
+
+    /**
+     * Counts the total number of documents matching the query.
+     * @returns {Promise<number>} - The total number of matching documents
+     */
+    async count() {
+        const totalDocs = await this.query.model.countDocuments(this.query.getQuery());
+        return totalDocs;
+    }
+}
 
 const queryClientFields = {
     name: 1,
@@ -20,9 +133,6 @@ const queryClientFields = {
 };
 
 const transformQuery = (query) => {
-    const queryCopy = { ...query };
-    //Delete key not attribute or variant
-
     const attributeQueries = [];
     const variantQueries = [];
     Object.keys(query).forEach((key) => {
@@ -57,7 +167,7 @@ const populateBrand = {
     path: "brandId",
     model: "Brand",
 };
-const clientRequiredFields = { isDeleted: false, isAvailable: true, isHide: false };
+const clientRequiredFields = {};
 
 // query attribute conversion function for attribute and variant
 
@@ -85,7 +195,7 @@ const getAllProducts = async (req, res, next) => {
     const populateVariantAndFilter = {
         ...populateVariation,
     };
-    const features = APIQuery(
+    const features = new APIQuery(
         Product.find({ ...clientRequiredFields, ...queryTransformed.attributeQuery, ...queryTransformed.variantQuery })
             .populate(populateVariantAndFilter)
             .select(queryClientFields)
